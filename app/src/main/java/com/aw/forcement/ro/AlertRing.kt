f9d894ca4ae300484f32dc0e0024dab3cf386b5a
Mac <wanjalaalex25@gmail.com>
2023-08-14 01:27:08 +0300

    dhdhhd

--- app/src/main/java/com/aw/forcement/ro/AlertRing.kt
+++ app/src/main/java/com/aw/forcement/ro/AlertRing.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.aw.forcement.R
 import com.aw.passanger.api.*
@@ -20,31 +22,55 @@ class AlertRing : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.alert_ring)
 
-        val name = intent.getStringExtra("names")!!.split(" ")[0]
+        val name = intent.getStringExtra("names")!!.split(" ")[0].lowercase().split(" ").joinToString(" ") { it.capitalize() }
 
         tv_title.text = "Alert " + name+"!"
-
-        "To alert ${name} to stay active, click the button below and his phone will ring."
+        tv_message.text =  "To alert ${name} to stay active, click the button below and his phone will ring."
 
         tv_close.setOnClickListener { finish() }
-        tv_ring.setOnClickListener {
 
-            try {
-                val am = getSystemService(AUDIO_SERVICE) as AudioManager
-                am.setStreamVolume(
-                    AudioManager.STREAM_MUSIC,
-                    am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
-                    0
-                )
-                val mPlayer2: MediaPlayer = MediaPlayer.create(this, R.raw.timer)
+        try {
+            val am = getSystemService(AUDIO_SERVICE) as AudioManager
+            am.setStreamVolume(
+                AudioManager.STREAM_MUSIC,
+                am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                0
+            )
+            val mPlayer2: MediaPlayer = MediaPlayer.create(this, R.raw.timer)
+
+            tv_ring.setOnClickListener {
+                layoutTime.visibility = View.VISIBLE
+
+                tv_title.text = "Alerting " + name+"!"
+
                 mPlayer2.start()
 
-            } catch (e: Exception) {
-                e.printStackTrace()
+                object : CountDownTimer(10000, 1000) {
+
+                    override fun onTick(millisUntilFinished: Long) {
+                        tv_timer.text = (millisUntilFinished / 1000).toString()
+                        if((millisUntilFinished/1000).toInt()==5){
+                            mPlayer2.start()
+                        }
+                    }
+
+                    override fun onFinish() {
+                        tv_message.text =  "You have successfully alerted ${name} phone. Keep track of his activities and intervene if needed."
+                        tv_ring.setOnClickListener { finish() }
+                        tv_ring.text = "close"
+                        mPlayer2.start()
+                        layoutTime.visibility = View.GONE
+                    }
+                }.start()
+
             }
 
 
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+
+
 
     }
 }
